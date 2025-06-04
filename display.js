7fb6203b0440f21fa21944654a998baf8db01e09
Saranya Muthaiyan <saramuthaiyan5@gmail.com>
2025-06-04 01:32:52 -0400

    changes

--- display.js
+++ display.js
@@ -4,11 +4,12 @@ export function displayMovies(movies) {
 
 
     movies.forEach(movie => {
+        const poster = movie.poster !== "N/A"? movie.poster :"https://via.placeholder.com/150";
         const card = document.createElement('div');
         card.classList.add('movie-card');
         card.innerHTML = `
         <h3>${movie.Title}</h3>
-        <img src = "${movie.Poster}" alt = "${movie.Title}" width = "100">
+        <img src = "${movie.Poster}" alt = "${movie.Title}" width = "150">
          <p>Year: ${movie.Year}</p>
         `;
          gallery.appendChild(card);
